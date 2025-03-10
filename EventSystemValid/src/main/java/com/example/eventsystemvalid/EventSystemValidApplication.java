08ac0590fee26539d89448b8e406d60d293cbbb0
AbrarSaud <abrar.sauod0@gmail.com>
2025-03-10 21:42:09 +0300

    first commit

---
+++ EventSystemValid/src/main/java/com/example/eventsystemvalid/EventSystemValidApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsystemvalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSystemValidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSystemValidApplication.class, args);
+    }
+
+}
---
+++ EventSystemValid/src/test/java/com/example/eventsystemvalid/EventSystemValidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsystemvalid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSystemValidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
