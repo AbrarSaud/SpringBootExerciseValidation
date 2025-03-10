ae48b7c7c6b29c535608fd6ec6bfe5437105ee17
AbrarSaud <abrar.sauod0@gmail.com>
2025-03-10 13:13:25 +0300

    first commit

---
+++ ProjectTrackerSystemValid/src/main/java/com/example/projecttrackersystemvalid/ProjectTrackerSystemValidApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttrackersystemvalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackerSystemValidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTrackerSystemValidApplication.class, args);
+    }
+
+}
---
+++ ProjectTrackerSystemValid/src/test/java/com/example/projecttrackersystemvalid/ProjectTrackerSystemValidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttrackersystemvalid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerSystemValidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
